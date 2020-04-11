32becc7c90fd369452e000cd4f5092b8f140705f
FrozenMango747 <59208086+FrozenMango747@users.noreply.github.com>
2020-04-11 11:25:50 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,10 +1,10 @@
 document.write(`<div class="displaytitle">
-<h1  align="center" ><a style="text-decoration:none;color:#000000" href="index.html">Frozen Mango 747</a></h1>
+<h1  align="center" ><a style="text-decoration:none;color:#000000" href="https://frozenmango747.github.io/">Frozen Mango 747</a></h1>
 </div><div class="topnav">
   <!-- add class="active" to current page-->
-  <a id="homeM" href="index.html">Home</a>
-  <a id="ifgM" href="IF-games.html">IF Games</a>
-  <a id="mcM" href="math-courses.html">Math Courses</a>
-  <a id="thM" href="treasure-hunt.html">Treasure Hunt</a>
-  <a id="abtM" href="about.html">About</a>
+  <a id="homeM" href="https://frozenmango747.github.io/">Home</a>
+  <a id="ifgM" href="IF-games">IF Games</a>
+  <a id="mcM" href="math-courses">Math Courses</a>
+  <a id="thM" href="treasure-hunt">Treasure Hunt</a>
+  <a id="abtM" href="about">About</a>
 </div>`)
