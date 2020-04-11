c4bc824b91824669783b19b0acf6e01241434882
FrozenMango747 <59208086+FrozenMango747@users.noreply.github.com>
2020-04-11 11:32:06 -0700

    Update menu.js

--- math-courses/cryptology/menu.js
+++ math-courses/cryptology/menu.js
@@ -1,19 +1,19 @@
 document.write(`<div class="displaytitle">
-<h1  align="center" ><a style="text-decoration:none;color:#000000" href="home.html">SOCRATHEMATICS</a></h1>
+<h1  align="center" ><a style="text-decoration:none;color:#000000" href="http://frozenmango747.github.io/math-courses/home">SOCRATHEMATICS</a></h1>
 </div><div class="topnav">
   <!-- add class="active" to current page-->
-  <a id="homeM" href="http://localhost:63342/websites/maingit/math-courses/home.html">Home</a>
-  <a id="trigM" href="http://localhost:63342/websites/maingit/math-courses/trigonometry.html">Trigonometry</a>
-  <a id="maM" href="http://localhost:63342/websites/maingit/math-courses/mod-arithmetic.html">Modular Arithmetic</a>
-  <a class="active" id="crM" href="http://localhost:63342/websites/maingit/math-courses/cryptology.html">Cryptology</a>
-  <a id="abtM" href="http://localhost:63342/websites/maingit/math-courses/about.html">About</a>
-  <a style="float:right;" id="ripe" href="http://localhost:63342/websites/maingit/index.html">Frozen Mango 747</a>
+  <a id="homeM" href="http://frozenmango747.github.io/math-courses/home">Home</a>
+  <a id="trigM" href="http://frozenmango747.github.io/math-courses/trigonometry">Trigonometry</a>
+  <a id="maM" href="http://frozenmango747.github.io/math-courses/mod-arithmetic">Modular Arithmetic</a>
+  <a id="crM" href="http://frozenmango747.github.io/math-courses/cryptology">Cryptology</a>
+  <a id="abtM" href="http://frozenmango747.github.io/math-courses/about">About</a>
+  <a style="float:right;" id="ripe" href="http://frozenmango747.github.io/">Frozen Mango 747</a>
 </div>
 <div class="topnav2">
   <!-- add class="active" to current page-->
-  <a id="plfM" href="http://localhost:63342/websites/maingit/math-courses/cryptology/playfair.html">Playfair</a>
-  <a id="rsM" href="http://localhost:63342/websites/maingit/math-courses/cryptology/rsa.html">RSA</a>
-  <a id="adfM" href="http://localhost:63342/websites/maingit/math-courses/cryptology/adfgvx.html">ADFGVX</a>
-  <a id="enM" href="http://localhost:63342/websites/maingit/math-courses/cryptology/enigma.html">Engima</a>
-  <a id="whyM" href="http://localhost:63342/websites/maingit/math-courses/cryptology/why.html">Why?</a>
+  <a id="plfM" href="http://frozenmango747.github.io/math-courses/cryptology/playfair">Playfair</a>
+  <a id="rsM" href="http://frozenmango747.github.io/math-courses/cryptology/rsa">RSA</a>
+  <a id="adfM" href="http://frozenmango747.github.io/math-courses/cryptology/adfgvx">ADFGVX</a>
+  <a id="enM" href="http://frozenmango747.github.io/math-courses/cryptology/enigma">Engima</a>
+  <a id="whyM" href="http://frozenmango747.github.io/math-courses/cryptology/why">Why?</a>
 </div>`)
