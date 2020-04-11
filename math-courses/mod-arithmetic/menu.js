aaedcad00fa5947fa6f5a7b9aa92666056e94a90
FrozenMango747 <59208086+FrozenMango747@users.noreply.github.com>
2020-04-11 11:29:05 -0700

    Update menu.js

--- math-courses/mod-arithmetic/menu.js
+++ math-courses/mod-arithmetic/menu.js
@@ -1,18 +1,18 @@
 document.write(`<div class="displaytitle">
-<h1  align="center" ><a style="text-decoration:none;color:#000000" href="home.html">SOCRATHEMATICS</a></h1>
+<h1  align="center" ><a style="text-decoration:none;color:#000000" href="http://frozenmango747.github.io/math-courses/home">SOCRATHEMATICS</a></h1>
 </div><div class="topnav">
   <!-- add class="active" to current page-->
-  <a id="homeM" href="http://localhost:63342/websites/maingit/math-courses/home.html">Home</a>
-  <a id="trigM" href="http://localhost:63342/websites/maingit/math-courses/trigonometry.html">Trigonometry</a>
-  <a class="active" id="maM" href="http://localhost:63342/websites/maingit/math-courses/mod-arithmetic.html">Modular Arithmetic</a>
-  <a id="crM" href="http://localhost:63342/websites/maingit/math-courses/cryptology.html">Cryptology</a>
-  <a id="abtM" href="http://localhost:63342/websites/maingit/math-courses/about.html">About</a>
-  <a style="float:right;" id="ripe" href="http://localhost:63342/websites/maingit/index.html">Frozen Mango 747</a>
+  <a id="homeM" href="http://frozenmango747.github.io/math-courses/home">Home</a>
+  <a id="trigM" href="http://frozenmango747.github.io/math-courses/trigonometry">Trigonometry</a>
+  <a class="active" id="maM" href="http://frozenmango747.github.io/math-courses/mod-arithmetic">Modular Arithmetic</a>
+  <a id="crM" href="http://frozenmango747.github.io/math-courses/cryptology">Cryptology</a>
+  <a id="abtM" href="http://frozenmango747.github.io/math-courses/about">About</a>
+  <a style="float:right;" id="ripe" href="http://frozenmango747.github.io/">Frozen Mango 747</a>
 </div>
 <div class="topnav2">
   <!-- add class="active" to current page-->
-  <a id="plfM" href="http://localhost:63342/websites/maingit/math-courses/mod-arithmetic/thinking-like-a-clock.html">Introduction</a>
-  <a id="rsM" href="http://localhost:63342/websites/maingit/math-courses/mod-arithmetic/basics.html">Basics</a>
-  <a id="adfM" href="http://localhost:63342/websites/maingit/math-courses/cryptology/adfgvx.html">Inverses</a>
-  <a id="enM" href="http://localhost:63342/websites/maingit/math-courses/cryptology/enigma.html">Applications</a>
+  <a id="plfM" href="http://frozenmango747.github.io/math-courses/mod-arithmetic/thinking-like-a-clock">Introduction</a>
+  <a id="rsM" href="http://frozenmango747.github.io/math-courses/mod-arithmetic/basics">Basics</a>
+  <a id="adfM" href="http://frozenmango747.github.io/math-courses/cryptology/adfgvx">Inverses</a>
+  <a id="enM" href="http://frozenmango747.github.io/math-courses/cryptology/enigma">Applications</a>
 </div>`)
