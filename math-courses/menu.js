2e5aa005dc8963c4e3f086f16c50997c2f34771c
FrozenMango747 <59208086+FrozenMango747@users.noreply.github.com>
2020-04-11 11:24:40 -0700

    Update menu.js

--- math-courses/menu.js
+++ math-courses/menu.js
@@ -2,10 +2,10 @@ document.write(`<div class="displaytitle">
 <h1  align="center" ><a style="text-decoration:none;color:#000000" href="home.html">SOCRATHEMATICS</a></h1>
 </div><div class="topnav">
   <!-- add class="active" to current page-->
-  <a id="homeM" href="home.html">Home</a>
-  <a id="trigM" href="trigonometry.html">Trigonometry</a>
-  <a id="maM" href="mod-arithmetic.html">Modular Arithmetic</a>
-  <a id="crM" href="cryptology.html">Cryptology</a>
-  <a id="abtM" href="about.html">About</a>
-  <a style="float:right;" id="ripe" href="http://localhost:63342/websites/maingit/index.html">Frozen Mango 747</a>
+  <a id="homeM" href="home">Home</a>
+  <a id="trigM" href="trigonometry">Trigonometry</a>
+  <a id="maM" href="mod-arithmetic">Modular Arithmetic</a>
+  <a id="crM" href="cryptology">Cryptology</a>
+  <a id="abtM" href="about">About</a>
+  <a style="float:right;" id="ripe" href="http://frozenmango747.github.io/">Frozen Mango 747</a>
 </div>`)
