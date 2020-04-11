04cf32072e7bccb9583616528d58c02ed037a232
FrozenMango747 <59208086+FrozenMango747@users.noreply.github.com>
2020-04-11 11:26:53 -0700

    Update menu.js

--- math-courses/menu.js
+++ math-courses/menu.js
@@ -1,11 +1,11 @@
 document.write(`<div class="displaytitle">
-<h1  align="center" ><a style="text-decoration:none;color:#000000" href="home.html">SOCRATHEMATICS</a></h1>
+<h1  align="center" ><a style="text-decoration:none;color:#000000" href="http://frozenmango747.github.io/math-courses/home">SOCRATHEMATICS</a></h1>
 </div><div class="topnav">
   <!-- add class="active" to current page-->
-  <a id="homeM" href="home">Home</a>
-  <a id="trigM" href="trigonometry">Trigonometry</a>
-  <a id="maM" href="mod-arithmetic">Modular Arithmetic</a>
-  <a id="crM" href="cryptology">Cryptology</a>
-  <a id="abtM" href="about">About</a>
+  <a id="homeM" href="http://frozenmango747.github.io/math-courses/home">Home</a>
+  <a id="trigM" href="http://frozenmango747.github.io/math-courses/trigonometry">Trigonometry</a>
+  <a id="maM" href="http://frozenmango747.github.io/math-courses/mod-arithmetic">Modular Arithmetic</a>
+  <a id="crM" href="http://frozenmango747.github.io/math-courses/cryptology">Cryptology</a>
+  <a id="abtM" href="http://frozenmango747.github.io/math-courses/about">About</a>
   <a style="float:right;" id="ripe" href="http://frozenmango747.github.io/">Frozen Mango 747</a>
 </div>`)
