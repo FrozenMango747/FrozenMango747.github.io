44550d3418e0cbdedb460110abda276f2b586c6d
FrozenMango747 <59208086+FrozenMango747@users.noreply.github.com>
2020-04-13 22:02:57 -0700

    Create menu.js

---
+++ finger-cipher/menu.js
@@ -0,0 +1,15 @@
+document.write(`<div class="displaytitle">
+<h1  align="center" ><a style="text-decoration:none;color:#000000" href="http://frozenmango747.github.io/math-courses/home">SOCRATHEMATICS</a></h1>
+</div><div class="topnav">
+  <!-- add class="active" to current page-->
+  <a id="homeM" href="http://frozenmango747.github.io/math-courses/home">Home</a>
+  <a id="trigM" href="http://frozenmango747.github.io/math-courses/trigonometry">Trigonometry</a>
+  <a id="maM" href="http://frozenmango747.github.io/math-courses/mod-arithmetic">Modular Arithmetic</a>
+  <a id="crM" href="http://frozenmango747.github.io/math-courses/cryptology">Cryptology</a>
+  <a id="abtM" href="http://frozenmango747.github.io/math-courses/about">About</a>
+  <a style="float:right;" id="ripe" href="http://frozenmango747.github.io/">Frozen Mango 747</a>
+</div>
+<div class="topnav2">
+<a id="thM" href="http://frozenmango747.github.io/finger-cipher/encryption-tool">Encryption</a>
+  <a id="abtM" href="http://frozenmango747.github.io/finger-cipher/about">About</a>
+</div>`)
